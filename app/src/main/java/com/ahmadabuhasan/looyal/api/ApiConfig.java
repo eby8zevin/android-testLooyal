6e2e80933cd7f6a5c0be80ca315c556f32afd690
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-05-13 05:57:31 +0700

    ApiConfig

---
+++ app/src/main/java/com/ahmadabuhasan/looyal/api/ApiConfig.java
@@ -0,0 +1,31 @@
+package com.ahmadabuhasan.looyal.api;
+
+import com.ahmadabuhasan.looyal.BuildConfig;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiConfig {
+
+    public static ApiService getApiService() {
+        HttpLoggingInterceptor loggingInterceptor;
+        if (BuildConfig.DEBUG) {
+            loggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+        } else {
+            loggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE);
+        }
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BuildConfig.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+        return retrofit.create(ApiService.class);
+    }
+}
