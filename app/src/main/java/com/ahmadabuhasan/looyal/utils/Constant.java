cf29ebc150b4b971e52d2d232241bdd0b3a59eda
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-05-13 05:59:37 +0700

    Constant Variable

---
+++ app/src/main/java/com/ahmadabuhasan/looyal/utils/Constant.java
@@ -0,0 +1,17 @@
+package com.ahmadabuhasan.looyal.utils;
+
+public class Constant {
+
+    public static String PREFS_NAME = "looyal";
+
+    public static String KEY_LOGIN = "key_login";
+    public static String KEY_USERNAME = "key_username";
+    public static String KEY_PASSWORD = "key_password";
+    public static String KEY_TEXT_AREA = "key_text_area";
+    public static String KEY_DATE_REGISTER = "key_date_register";
+
+    public static String DATA_CODE = "data_code";
+    public static String DATA_NAME = "name";
+    public static String DATA_STATE_CODE = "state_code";
+    public static String DATA_STATE_NAME = "state_name";
+}
